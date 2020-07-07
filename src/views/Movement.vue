65f63025f1960d8d233339b82350100ba5b9e8e7
Jorge Calatayud <jorge.cmaeso@gmail.com>
2020-07-07 22:20:58 +0200

    Movement Panel Move Axis and methods tested

--- src/views/Movement.vue
+++ src/views/Movement.vue
@@ -20,9 +20,7 @@
 					v-model="movementModeSelected"
 					:items="movementModes"
 				></v-combobox>
-                <v-btn>
-                    Mover Eje
-                </v-btn>
+				<v-btn @click="moveAxis">Mover Eje</v-btn>
 			</v-col>
 			<v-col sm="8" v-if="movementModeSelected.mode === 'MT'">
 				<v-card>
@@ -65,18 +63,18 @@
 							></v-text-field>
 						</v-col>
 					</v-row>
-                    <v-row justify="space-around" dense>
-                        <v-col sm="4">
-                            <p>Position: {{currentPosition}}</p>
-                        </v-col>
-                        <v-col sm="4">
-                            <v-btn>Read Position</v-btn>
-                        </v-col>
-                    </v-row>
+					<v-row justify="space-around" dense>
+						<v-col sm="4">
+							<p>Position: {{currentPosition}}</p>
+						</v-col>
+						<v-col sm="4">
+							<v-btn>Read Position</v-btn>
+						</v-col>
+					</v-row>
 				</v-card>
 			</v-col>
 			<v-col sm="8" v-else-if="movementModeSelected.mode === 'MR'">
-            <v-card>
+				<v-card>
 					<v-row justify="space-around" dense>
 						<v-col sm="4">
 							<v-combobox
@@ -115,9 +113,9 @@
 								:rules="[positionRule('movementEndPosition')]"
 								v-on:blur="movementTargetPositionCorrect('movementEndPosition')"
 							></v-text-field>
-                            <v-text-field
+							<v-text-field
 								class="input-centered"
-								v-model="movementTargetPosition"
+								v-model="movementIncrement"
 								label="Incremento Angular"
 								type="number"
 								outlined
@@ -127,7 +125,7 @@
 						</v-col>
 					</v-row>
 				</v-card>
-                </v-col>
+			</v-col>
 		</v-row>
 	</v-container>
 </template>
@@ -159,7 +157,7 @@ export default {
 						{ number: 6, min: 0, max: 155 }
 					]
 				}
-            ],
+			],
 			systemSelected: {},
 			movementModes: [
 				{ mode: "MT", name: "Modo Track" },
@@ -173,20 +171,20 @@ export default {
 				{ mode: "d", name: "Auto" }
 			],
 			movementDirectionSelected: {},
-			movementSpeed: 90,
-            movementTargetPosition: 180,
-            currentPosition: 0,
-            movementStartPosition: 0,
-            movementEndPosition: 180,
-            movementIncrement: 0.1
+			movementSpeed: "90.0",
+			movementTargetPosition: "180.00",
+			currentPosition: "0.00",
+			movementStartPosition: "0.00",
+			movementEndPosition: "180.00",
+			movementIncrement: "0.100"
 		};
 	},
 	methods: {
 		speedRule(speed) {
-            if(isNaN(speed)){
-                return "La velocidad no es un numero"
-            }
-            speed = parseFloat(speed)
+			if (isNaN(speed)) {
+				return "La velocidad no es un numero";
+			}
+			speed = parseFloat(speed);
 			if (speed > 92) {
 				return "La velocidad no puede ser superior a 92%";
 			} else if (speed <= 0) {
@@ -195,83 +193,107 @@ export default {
 			return true;
 		},
 		positionRule(positionKey) {
-            let position = this[positionKey]
-            if(isNaN(position)){
-                return "La posición no es un numero"
-            }
-            position =  parseFloat(position)
-            if(position < this.axisSelected.min){
-                return `La posición es menor que el límite inferior ${this.axisSelected.min}`
-            }else if(position > this.axisSelected.max){
-                return `La posición es mayor que el límite superior ${this.axisSelected.max}`
-            }
+			let position = this[positionKey];
+			if (isNaN(position)) {
+				return "La posición no es un numero";
+			}
+			position = parseFloat(position);
+			if (position < this.axisSelected.min) {
+				return `La posición es menor que el límite inferior ${this.axisSelected.min}`;
+			} else if (position > this.axisSelected.max) {
+				return `La posición es mayor que el límite superior ${this.axisSelected.max}`;
+			}
 			return true;
-        },
-        /* eslint-disable-next-line */
-        incrementRule(increment){
-            if(isNaN(increment)){
-                return "El incremento no es un número"
-            }
-            increment = parseFloat(increment)
-            if(parseFloat(this.movementEndPosition) % increment !== 0){
-                return "La poscición final no es múltiplo del incremento"
-            }else if(parseFloat(this.movementStartPosition) % increment !== 0){
-                return "La posición inicial no el múltiplo del incremento"
-            }else if(increment < 0.005){
-                return "El incremento no puede ser menor que 0.005º"
-            }else if(increment > 49.999){
-                return "El incremento no puede ser mayor que 49.999º"
-            }
-            return true
-        },
-        movementIncrementCorrect(){
-            /* eslint-disable-next-line */
-            if(!!this.incrementRule(this.movementIncrement)){
-                this.movementIncrement = 0.005
-                return
-            }
-            this.movementIncrement = parseFloat(this.movementIncrement).toFixed(3);
-        },
+		},
+		/* eslint-disable-next-line */
+		incrementRule(increment) {
+			if (isNaN(increment)) {
+				return "El incremento no es un número";
+			}
+			increment = parseFloat(increment).toFixed(3);
+            if (increment < 0.005) {
+				return "El incremento no puede ser menor que 0.005º";
+			} else if (increment > 49.999) {
+				return "El incremento no puede ser mayor que 49.999º";
+			} else if(((parseFloat(this.movementEndPosition)-parseFloat(this.movementStartPosition))*1000) % (increment*1000) !== 0) {
+				return "Las posiciones no son multiplo del incremento";
+			}
+			return true;
+		},
+		movementIncrementCorrect() {
+			if (this.incrementRule(this.movementIncrement) !== true) {
+				this.movementIncrement = 0.005;
+				return;
+			}
+			this.movementIncrement = parseFloat(this.movementIncrement).toFixed(3);
+		},
 		movementSpeedCorrect() {
 			if (this.speedRule(this.movementSpeed) !== true) {
 				this.movementSpeed = 90;
 				return;
 			}
 			this.movementSpeed = parseFloat(this.movementSpeed).toFixed(1);
-        },
+		},
 		movementTargetPositionCorrect(key) {
-            console.log(this[key])
+			console.log(this[key]);
 			if (this.positionRule(key) !== true) {
 				this[key] = 0;
 				return;
-            } 
+			}
 			this[key] = parseFloat(this[key]).toFixed(2);
-        },
+		},
 
-        async readPosition(){
+		async readPosition() {
+			/* eslint-disable-next-line */
+			if (typeof pywebview !== "undefined") {
+				/* eslint-disable-next-line */
+				this.currentPosition = await pywebview.api.PyReadPos(
+					this.axisSelected.number
+				);
+			}
+		},
+		async moveAxis() {
             /* eslint-disable-next-line */
-            if (typeof pywebview !== "undefined") {
-                /* eslint-disable-next-line */
-                this.currentPosition = await pywebview.api.PyReadPos(this.axisSelected.number);
-            }
-        }    
-
+			if (typeof pywebview !== "undefined") {
+				switch (this.movementModeSelected.mode) {
+                    case "MT":
+                        /* eslint-disable-next-line */
+                        await pywebview.api.PyMoveTrack({
+                            direction: this.movementDirectionSelected.mode,
+                            speed: this.movementSpeed,
+                            target: this.movementTargetPosition,
+                            axis: this.axisSelected.number
+                        })
+						break;
+					case "MR":
+                        /* eslint-disable-next-line */
+                        await pywebview.api.PyMoveTrack({
+                            speed: this.movementSpeed,
+                            startpos: this.movementStartPosition,
+                            endpos: this.movementEndPosition,
+                            increment: this.movementIncrement,
+                            axis: this.axisSelected.number
+                        })
+						break;
+				}
+			}
+		}
 	},
 	watch: {
 		systemSelected: function(val) {
 			this.systemSelected = val;
-        },
-        axisSelected: function(val){
-            this.axisSelected = val;
-            this.readPosition();
-        }
+		},
+		axisSelected: function(val) {
+			this.axisSelected = val;
+			this.readPosition();
+		}
 	},
 	created() {
 		this.systemSelected = this.systems[0];
 		this.movementModeSelected = this.movementModes[0];
 		this.axisSelected = this.systemSelected.axes[0];
-        this.movementDirectionSelected = this.movementDirection[0];
-        this.readPosition()
+		this.movementDirectionSelected = this.movementDirection[0];
+		this.readPosition();
 	}
 };
 </script>
